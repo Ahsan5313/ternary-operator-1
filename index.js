82ccdf8ab64cbe6e4ad3e078cccd6ca7b7ecd517
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 13:39:35 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var Num1=Number(prompt("Enter 1st number : "));
+var Num2=Number(prompt("Entere 2nd numbe :  "));
+var Num3=Number(prompt("Enter 3rd number : "));
+var result=Num1>Num2 && Num1>Num3?"Num1 is large ":Num2>Num3 && Num2>Num1?"Num2 is large":"Num3 is large";
+document.write(result);
+
+
